dfe1710128be3f92927f15bf16a0076a266555ef
wuxiwen <279712908@qq.com>
2017-04-26 10:11:40 +0800

    add

---
+++ xiaoNS/xiaoNS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  xiaoNS
+//
+//  Created by 希文 on 2017/4/26.
+//  Copyright © 2017年 xiwen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ xiaoNS/xiaoNS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  xiaoNS
+//
+//  Created by 希文 on 2017/4/26.
+//  Copyright © 2017年 xiwen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
