fd4367820e934f7c3b0fdedf293a259d5cfb36ed
wuxiwen <279712908@qq.com>
2017-05-15 09:11:07 +0800

    add

--- xiaoNS/xiaoNS/NPictureController.m
+++ xiaoNS/xiaoNS/NPictureController.m
@@ -8,6 +8,8 @@
 
 #import "NPictureController.h"
 
+#import "UIImageView+WebCache.h"
+
 #import "subPicture.h"
 
 @interface NPictureController ()
@@ -23,6 +25,7 @@
     self.view.backgroundColor = [UIColor blueColor];
     // Do any additional setup after loading the view.
     NSLog(@"%@", self.imgs);
+    
     [self setSubPictures];
     [self setAllImages];
 }
@@ -43,7 +46,44 @@
 }
 
 - (void)setAllImages {
-    UIImageView *img = [[UIImageView alloc] init];
+    
+    CGFloat viewW = self.view.frame.size.width;
+    CGFloat viewH = self.view.frame.size.height;
+    // 加载一个滚动视图
+    UIScrollView *scrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 0, viewW, viewH)];
+    scrollView.backgroundColor = [UIColor whiteColor];
+    [self.view addSubview:scrollView];
+    
+    
+    // 加载图片视图
+    
+    CGFloat imageW = (viewW - 4 * 10) / 3;
+    CGFloat imageH = imageW;
+    
+    CGFloat scrollH = viewH;
+    for (int i=0; i<self.subPictures.count; i++) {
+        int ii = i%3;
+        int jj = i/3;
+        CGFloat imageX = 10 + (imageW + 10) * ii;
+        CGFloat imageY = 10 + (imageH + 10) * jj;
+        UIImageView *img = [[UIImageView alloc] init];
+        subPicture *subPic = self.subPictures[i];
+        NSURL *url = [NSURL URLWithString:[NSString stringWithFormat:@"http://cloudowr.oss-cn-hangzhou.aliyuncs.com/nsgcgl/420921/%@?x-oss-process=style/thumb_100_100", subPic.url]];
+        [img sd_setImageWithURL:url];
+        img.frame = CGRectMake(imageX, imageY, imageW, imageH);
+        
+        [scrollView addSubview:img];
+        
+        // 设置滚动区域
+        CGFloat maxH = CGRectGetMaxY(img.frame) + imageW;
+        scrollH = scrollH > maxH ? scrollH : maxH;
+    }
+    NSLog(@"%f", scrollH);
+    
+    scrollView.contentSize = CGSizeMake(0, scrollH);
+    
+    
+    
 }
 
 
