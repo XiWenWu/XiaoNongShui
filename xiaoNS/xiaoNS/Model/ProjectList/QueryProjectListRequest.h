0c054fc1d9ea72a2c41d2b6d4452197c25073271
wangcq <wangcq@114995.com>
2017-05-17 18:10:15 +0800

    modify

---
+++ xiaoNS/xiaoNS/Model/ProjectList/QueryProjectListRequest.h
@@ -0,0 +1,16 @@
+//
+//  QueryProjectListRequest.h
+//  xiaoNS
+//
+//  Created by wcq on 17/5/17.
+//  Copyright © 2017年 xiwen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface QueryProjectListRequest : NSObject
+
+@property (nonatomic, strong) NSNumber *userId;
+@property (nonatomic, strong) NSNumber *projectIndex;
+
+@end
---
+++ xiaoNS/xiaoNS/Net/Url.h
@@ -0,0 +1,19 @@
+//
+//  Url.h
+//  xiaoNS
+//
+//  Created by wcq on 17/5/17.
+//  Copyright © 2017年 xiwen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Url : NSObject
+
+
+//工程信息
+#define kNsgcglapiV3ProjectList @"/nsgcgl/api/v3/projectList"
+
+
+
+@end
